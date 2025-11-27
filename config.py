bff6263f8ea45ef107ece4afaded8d6c15a8bba7
Ritesh Traipathi <ritesh6263tripathi@gmail.com>
2025-11-27 14:10:09 +0530

    Add configuration for environment variables

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
+PINECONE_VECTOR_DIM = int(os.getenv("PINECONE_VECTOR_DIM", 1536))
+NEO4J_URI = os.getenv("NEO4J_URI")
+NEO4J_USER = os.getenv("NEO4J_USER")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
